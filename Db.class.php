6a35925789bd20e09376872c7710dbb12577a1b0
Indieteq <admin@indieteq.com>
2012-10-17 22:50:54 +0300

    Update Db.class.php

--- Db.class.php
+++ Db.class.php
@@ -4,7 +4,7 @@
 *
 * @author		Author: Vivek Wicky Aswal. (https://twitter.com/#!/VivekWickyAswal)
 * @git 			htt://github.com/indieteq-vivek/simple-db-class
-* @version      0.2a
+* @version      0.2ab
 *
 */
 	require("Log.class.php");

af106b1cfb68d698c7b77f5c18cb60b84762969c
Indieteq <admin@indieteq.com>
2012-11-10 20:20:53 +0100

    Update easyCRUD/index.php

--- easyCRUD/index.php
+++ easyCRUD/index.php
@@ -1,39 +1,32 @@
 <?php
-	
 	require("Person.class.php");
 	
-	//$person  = new Person(array("Firstname"=>"kona","Age"=>"20","Sex"=>"F"));
-	//$person  = new Person(array("id"=>"67"));
+// Our person object
+	$person  = new Person();
 
 //	Create new person
-//	$person->Firstname = "Kona";
-//	$person->Age  = "20";
-//	$person->Sex = "F";
-//	$creation = $person->Create();
+	$person->Firstname = "Kona";
+	$person->Age  = "20";
+	$person->Sex = "F";
+	$creation = $person->Create();
 
-	//var_dump($creation);
 
 //	Update Person Info
-//	$person->id = "4";	
-//	$person->Age = "32";
-//	$saved = $person->Save(); 
-
-//	var_dump($saved);
+	$person->id = "4";	
+	$person->Age = "32";
+	$saved = $person->Save(); 
 
 //  Find person
-	//$person->id = "4";		
-	//$person->Find();
+	$person->id = "4";		
+	$person->Find();
 
-//	echo $person->Firstname;
-//	echo $person->Age;
+	echo $person->Firstname;
+	echo $person->Age;
 	
 // Delete person
-//	$person->id = "17";	
+	$person->id = "17";	
 	$delete = $person->Delete();
-	var_dump($delete);
 
  // Get all persons
 	$persons = $person->all();  
-
-
 ?>

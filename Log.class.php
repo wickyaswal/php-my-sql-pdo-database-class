b04cfd3daf8b547b22bdd7744e17a16ae7bd79ac
Indieteq <admin@indieteq.com>
2012-11-10 20:26:44 +0100

    Update Log.class.php

--- Log.class.php
+++ Log.class.php
@@ -1,5 +1,5 @@
 <?php 
-   /* *
+       /* *
 	* Log 			A logger class which creates logs when an exception is thrown.
 	* @author		Author: Vivek Wicky Aswal. (https://twitter.com/#!/VivekWickyAswal)
 	* @git 			htt://github.com/indieteq-vivek/simple-db-class
@@ -10,47 +10,47 @@
 		    # @string, Log directory name
 		    	private $path = '/logs/';
 			
-			# @void, Default Constructor, Sets the timezone and path of the log files.
-				public function __construct() {
-					date_default_timezone_set('Europe/Amsterdam');	
-					$this->path  = dirname(__FILE__)  . $this->path;	
-				}
+		    # @void, Default Constructor, Sets the timezone and path of the log files.
+			public function __construct() {
+				date_default_timezone_set('Europe/Amsterdam');	
+				$this->path  = dirname(__FILE__)  . $this->path;	
+			}
 			
 		   /**
 		    *   @void 
 		    *	Creates the log
 		    *
-			*   @param string $message the message which is written into the log.
-			*	@description:
-			*	 1. Checks if directory exists, if not, create one and call this method again.
-			*	 2. Checks if log already exists.
-			*	 3. If not, new log gets created. Log is written into the logs folder.
-			*	 4. Logname is current date(Year - Month - Day).
-			*	 5. If log exists, edit method called.
-			*	 6. Edit method modifies the current log.
-			*/	
-				public function write($message) {
-					$date = new DateTime();
-					$log = $this->path . $date->format('Y-m-d').".txt";
-	
-					if(is_dir($this->path)) {
-						if(!file_exists($log)) {
-							$fh  = fopen($log, 'w') or die("Fatal Error !");
-							$logcontent = "Time : " . $date->format('H:i:s')."\r\n" . $message ."\r\n";
-							fwrite($fh, $logcontent);
-							fclose($fh);
-						}
-						else {
-							$this->edit($log,$date, $message);
-						}
+		    *   @param string $message the message which is written into the log.
+		    *	@description:
+		    *	 1. Checks if directory exists, if not, create one and call this method again.
+	            *	 2. Checks if log already exists.
+		    *	 3. If not, new log gets created. Log is written into the logs folder.
+		    *	 4. Logname is current date(Year - Month - Day).
+		    *	 5. If log exists, edit method called.
+		    *	 6. Edit method modifies the current log.
+		    */	
+			public function write($message) {
+				$date = new DateTime();
+				$log = $this->path . $date->format('Y-m-d').".txt";
+
+				if(is_dir($this->path)) {
+					if(!file_exists($log)) {
+						$fh  = fopen($log, 'w') or die("Fatal Error !");
+						$logcontent = "Time : " . $date->format('H:i:s')."\r\n" . $message ."\r\n";
+						fwrite($fh, $logcontent);
+						fclose($fh);
 					}
 					else {
-						  if(mkdir($this->path,0777) === true) // Check on true, Otherwise we would have a never-ending loop :S
-						  {
-	 						 $this->write($message);  
-						  }	
+						$this->edit($log,$date, $message);
 					}
 				}
+				else {
+					  if(mkdir($this->path,0777) === true) // Check on true, Otherwise we would have a never-ending loop :S
+					  {
+ 						 $this->write($message);  
+					  }	
+				}
+			 }
 			
 			/** 
 			 *  @void
@@ -61,10 +61,10 @@
 			 * @param DateTimeObject $date
 			 * @param string $message
 			 */
-				private function edit($log,$date,$message) {
-					$logcontent = "Time : " . $date->format('H:i:s')."\r\n" . $message ."\r\n\r\n";
-					$logcontent = $logcontent . file_get_contents($log);
-					file_put_contents($log, $logcontent);
-				}
+			    private function edit($log,$date,$message) {
+				$logcontent = "Time : " . $date->format('H:i:s')."\r\n" . $message ."\r\n\r\n";
+				$logcontent = $logcontent . file_get_contents($log);
+				file_put_contents($log, $logcontent);
+			    }
 		}
 ?>

5938f993a966603ee92aa2e85a1c1a351ff30bae
Indieteq <admin@indieteq.com>
2013-10-18 18:26:33 +0200

    Fixed relative path problem
    It's now possible to require the easyCRUD class file from any file from any folder.

--- easyCRUD/easyCRUD.class.php
+++ easyCRUD/easyCRUD.class.php
@@ -5,7 +5,7 @@
 * @author		Author: Vivek Wicky Aswal. (https://twitter.com/#!/VivekWickyAswal)
 * @version      0.1a
 */
-require_once("../Db.class.php");
+require_once(__DIR__ . '/../Db.class.php');
 class Crud {
 
 	private $db;
